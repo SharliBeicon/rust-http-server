86ab24c8f201cfe454f6ed18aacc2afdef12042d
Charlie Bacon <carlos.tocino@payretailers.com>
2024-06-08 13:49:41 +0200

    / get response and 404

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs::{self},
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let length = content.len();
+
+    let response = format!(
+        "{status}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {content}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
